d2bf478ea82cba4b4d994890774cf7380138b17c
gauravchavhan05 <104491971+gauravchavhan05@users.noreply.github.com>
2023-09-17 15:25:24 +0530

    first commit

--- src/components/pages/home/AppLayout.jsx
+++ src/components/pages/home/AppLayout.jsx
@@ -2,7 +2,6 @@ import Hero from '../../hero/Hero'
 import Partner from '../../partner/Partner'
 import Slider from '../../slider/Slider'
 import Subscribe from '../../subscribe/Subscribe'
-import NavTwo from '../../vart-nav/NavTwo'
 import WhyChoose from '../../why-choose/WhyChoose'
 import './AppLayout.css'
 
